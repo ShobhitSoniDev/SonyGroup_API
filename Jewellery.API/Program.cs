35edc0e4d25d74159423c0d6ab15f2a2e64e0d33
ShobhitSoniDev <shobhitsoni995@outlook.com>
2026-05-17 11:56:28 +0530

    Update Program.cs

--- Jewellery.API/Program.cs
+++ Jewellery.API/Program.cs
@@ -139,14 +139,11 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // ------------------ Middleware ------------------
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jewellery API V1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jewellery API V1");
+});
 
 app.UseHttpsRedirection();
 
